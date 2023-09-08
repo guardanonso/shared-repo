30e24f10007476b1f2e5f35745a9c564bf15551d
Flavio Marolla <flavio.marolla1@gmail.com>
2023-09-08 20:30:00 +0200

    ex 4 corrected

--- python exercises/ex 4.py
+++ python exercises/ex 4.py
@@ -1,24 +1,20 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
-while True:
-    while True:
-        word = input("Type a word: ")
-        if word.isalpha()==False:
-            continue
-        else:   break
-    chars = int(input("How many characters you want to remove? "))
-    lenght = len(word)
-    if chars>lenght:
-        print("Select a number of character that's less than the lenght of the word")
-        continue
-    else:
-        break
-
 def remove_chars(word,chars):
     a = word[chars:]
     return(a)
 
-if remove_chars(word,chars)=="":
-    print("Resault: whole word cancelled")
-else:
-    print("Resault:",remove_chars(word,chars))
+while True:
+    inpword = input("Choose a word: ")
+    try:
+        inpchars = int(input("How many characters you want to delete? "))
+    except: 
+        print("Please enter a number")
+        continue
+    length = len(inpword)
+    if inpchars >= length:
+        print("Choose a number of characters that's lower than the length of thw word you chose")
+        continue
+    else:   
+        print(remove_chars(inpword,inpchars))
+
