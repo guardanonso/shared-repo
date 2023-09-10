60789e205d4fd7281ec4fe2cf1ff65fd029f4471
Flavio Marolla <flavio.marolla1@gmail.com>
2023-09-10 14:25:58 +0200

    balls scraping

--- ch_12/using_urllib.py
+++ ch_12/using_urllib.py
@@ -1,8 +1,18 @@
-from urllib import request, error, parse
+import requests
+from bs4 import BeautifulSoup
 
-fhand = request.urlopen("https://it.wikipedia.org/wiki/Evgenij_Prigo%C5%BEin")
+url = input("Type in an url: ")
 
+html = requests.get(url)
 
+soup = BeautifulSoup(html.content,"html.parser")
 
-a = fhand.read().decode()
-print(a)
+result = soup.find(id="ResultsContainer")
+
+job_elements = result.find_all("div", class_="card-content")
+
+for job_element in job_elements:
+    print(job_element, end = "\n\n")
+
+# https://www.trony.it/online
+# https://realpython.github.io/fake-jobs/
