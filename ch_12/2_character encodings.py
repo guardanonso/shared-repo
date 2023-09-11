b5edccb35609ca331a8785f7e237b4fbd65ef9bd
Flavio Marolla <flavio.marolla1@gmail.com>
2023-09-11 22:19:04 +0200

    yes

--- ch_12/2_character encodings.py
+++ ch_12/2_character encodings.py
@@ -1,12 +1,23 @@
 from urllib.request import urlopen
 
+# prints the body of the response in a file (html file) if the body uses utf-8 encoding
 with urlopen("https://www.example.com") as response:
     body = response.read()
-print(dir(response.headers))
 charset = response.headers.get_content_charset()
 
-print(charset)
 decoded_body = body.decode(charset)
+with open("document.html","wb") as file:
+    file.write(body)
 
-print(decoded_body)
+# prints the body of the response in a file (html file) if the body uses other kinds of encoding
+with urlopen("https://www.google.com") as gresponse:
+    gbody = gresponse.read()
+
+gcharset = gresponse.headers.get_content_charset()
+print(gcharset)
+gdecoded_body = gbody.decode(gcharset)
+
+with open("file2.html", encoding = "utf-8", mode = "w") as gfile:
+    gfile.write(gdecoded_body)
+
 
