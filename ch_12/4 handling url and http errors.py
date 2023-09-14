695375bc907d2fdc1bf896d3e9a86e48a8caf69c
Flavio Marolla <flavio.marolla1@gmail.com>
2023-09-14 12:06:02 +0200

    on it

--- ch_12/4 handling url and http errors.py
+++ ch_12/4 handling url and http errors.py
@@ -1,9 +1,24 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from urllib.error import URLError, HTTPError
+import json
+import ssl
 
-myurl = input("Enter a URL: ")
-def make_request(url):
-    with urlopen(url, timeout=10) as response:
-        print(response.status)
-        return(response.read(),response)
-make_request(myurl)
+def make_request(url, headers=None):
+    request = Request(url, headers = headers or {})
+    try: 
+        with urlopen(request, timeout=10) as response:
+            print(response.status)
+            return(response.read(),response)
+    except HTTPError as error:
+        print(error.status, error.reason)
+    except URLError as error:
+        print(error.reason)
+    except TimeoutError:
+        print("Request timed out")
+
+body, response = make_request("https://superfish.badssl.com/",
+                               {"User-Agent": "Real Python"})
+        
+print(response.getheaders())
+
+
