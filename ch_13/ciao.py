eb4d26a4a0dfec8756b329707888ebf8e29a9137
Flavio Marolla <flavio.marolla1@gmail.com>
2023-09-19 17:37:24 +0200

    ci siamo

--- ch_13/ciao.py
+++ ch_13/ciao.py
@@ -1,35 +1,62 @@
 from urllib import request, parse, error
 import json
+from datetime import date, datetime
+from datetime import timedelta
 
-inp = input("Choose between these options:\n1. Current weather in a given location \n2. Forecast weather\n3. History weather(previous 7 days in a given location)")
-current_serviceurl = "http://api.weatherapi.com/v1/current.json"
-history_url = "http://api.weatherapi.com/v1/hisotry.json"
+tod = date.today()
+date_format = "%Y-%m-%d"
+dtObj = datetime.strptime(str(tod), date_format)
+past_date = dtObj - timedelta(days=10)
+
+print(past_date.date())
+
+# inp = input("Choose between these options:\n1. Current weather in a given location \n2. Forecast weather\n3. History weather(previous 7 days in a given location)")
+current_url = "http://api.weatherapi.com/v1/current.json"
+history_url = "http://api.weatherapi.com/v1/history.json"
 future_url = "http://api.weatherapi.com/v1/future.json"
 
-url_dic = {"1" : current_serviceurl,"2" : history_url, "3" : future_url}
+url_dic = {"1" : current_url,"2" : history_url, "3" : future_url}
 
 api_key = "70054fa919c448b7992132453231809"
 location = input("Enter a location: ")
 
 
-def current_weather_data(api_url):
+# def current_weather_data(api_url):
 
-    query_params = parse.urlencode({"key":api_key,"q" : location})
-    print(query_params)
+#     query_params = parse.urlencode({"key":api_key,"q" : location})
+#     print(query_params)
+
+#     url = f"{api_url}?{query_params}"
+
+#     try:
+#         with request.urlopen(url) as uh:
+#             data = uh.read().decode()
+#             dic = json.loads(data)
+#         print(json.dumps(dic, indent = 4))
+
+#     except error.HTTPError as error:
+#         print(error)
+#     except error.URLError as error:
+#         print(error)
+
+def history_weather_data(api_url):
+    query_params = parse.urlencode({"key":api_key,"q":location, "dt" : past_date})
 
     url = f"{api_url}?{query_params}"
+    print(url)
 
     try:
         with request.urlopen(url) as uh:
             data = uh.read().decode()
             dic = json.loads(data)
-        print(json.dumps(dic, indent=4))
+        print(json.dumps(dic, indent = 4))
 
     except error.HTTPError as error:
         print(error)
     except error.URLError as error:
         print(error)
 
+history_weather_data(history_url)
 
 
 
---
+++ ch_13/hisotry.py
@@ -0,0 +1,47 @@
+from urllib import request, parse, error
+import json
+from datetime import date, datetime
+from datetime import timedelta
+
+history_url = "http://api.weatherapi.com/v1/history.json"
+api_key = "70054fa919c448b7992132453231809"
+location = input("Enter a location: ")
+
+while True:
+    days = input("How many days behind?: ")
+    if days.isdigit() and float(str(days)).is_integer() and int(days)<= 365:
+        break
+    else:   continue
+
+lst = list()
+for n in range(1,int(days)+1):
+    tod = date.today()
+    date_format = "%Y-%m-%d"
+    dtObj = datetime.strptime(str(tod), date_format)
+    past_date = dtObj - timedelta(days=n) 
+    dates = str(past_date.date())
+    print(dates,type(dates))
+    lst.append(dates)
+print(lst)
+    
+
+def history_weather_data(api_url):
+    for d in lst:
+        query_params = parse.urlencode({"key":api_key,"q":location, "dt" : d})
+
+        url = f"{api_url}?{query_params}"
+
+        try:
+            with request.urlopen(url,timeout=10) as uh:
+                data = uh.read().decode()
+                dic = json.loads(data)
+                print(dic["forecast"]["forecastday"][0]["day"]["avgtemp_c"])
+
+        except error.HTTPError as error:
+            print(error)
+        except error.URLError as error:
+            print(error)
+        except TimeoutError as error:
+            print(error)
+
+history_weather_data(history_url)
