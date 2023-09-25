f57f032d4656b65be6b2ee74e79c139aa3bf8d72
Flavio Marolla <flavio.marolla1@gmail.com>
2023-09-25 16:40:50 +0200

    hiiii

--- ch_13/authentication for weather api.py
+++ ch_13/authentication for weather api.py
@@ -1,1 +1,18 @@
 import requests
+
+URL = "https://www.weatherapi.com"
+playload = {"ctl00$MainContentHolder$Login1$UserName" : "flavio.marolla1@gmail.com",
+            "ctl00$MainContentHolder$Login1$Password" : "Ciaoiosonosveva1"}
+s = requests.session()
+
+response = s.post(URL, playload)
+
+content = response.content
+
+print(response.status_code)
+
+print(content)
+
+with open("non so.html","wb") as file:
+    file.write(content)
+
