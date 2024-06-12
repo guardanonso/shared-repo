e878c967f44c6a953579e1277ca47538726c25fb
Flavio Marolla <flavio.marolla1@gmail.com>
2024-06-12 18:14:54 +0200

    ciau

--- C/compute_volume.c
+++ C/compute_volume.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int height, weight, width;
+    int height, length, width;
     printf("Enter height of the box:\n");
     scanf("%d", &height);
-    printf("Enter the weight:\n");
-    scanf("%d", &weight);
+    printf("Enter the length:\n");
+    scanf("%d", &length);
     printf("Enter the width:\n");
     scanf("%d", &width);
 
-    printf("Volume: %d", height*weight*width);
+    printf("Volume: %d", height*length*width);
     return 0;
 }
