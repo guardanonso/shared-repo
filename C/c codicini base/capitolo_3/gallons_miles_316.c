aaca35c06ba34d40142c961f24851384269a8162
Flavio Marolla <flavio.marolla1@gmail.com>
2024-09-01 11:12:32 +0200

    a

---
+++ C/c codicini base/capitolo_3/gallons_miles_316.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void){
+    float gallons, miles, result;
+    float gallons_sum = 0;
+    float miles_sum = 0;
+
+    while(1){
+        printf("Enter the gallons used (-1 to end): ");
+        scanf("%f", &gallons);
+
+        if(gallons == -1){
+
+            if (gallons_sum > 0){
+                printf("The overall average miles/gallon was: %.2f", miles_sum/gallons_sum);   
+                }
+              
+            else{
+                printf("No gallons were entered, so I can't calculate the average miles/gallons.\n");
+            }    
+            break;
+        }
+
+        gallons_sum += gallons;
+
+        printf("Enter the miles driven: ");
+        scanf("%f", &miles);
+        miles_sum += miles;
+
+        result = miles/gallons;
+        printf("The miles/gallons is: %f\n", result);
+
+    }
+}
---
+++ C/c codicini base/capitolo_3/limite_credito.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int account_number;
+    float credit_limit, beginning_balance, total_charges, total_credits;
+
+    float balance;
+
+
+    while(1){
+        printf("Enter account number (-1 to end): ");
+        scanf("%d", &account_number);
+        if(account_number == -1){
+            break;
+        }
+
+        printf("Enter beginning balance: ");
+        scanf("%f", &beginning_balance);
+
+        printf("Enter total charges: ");
+        scanf("%f", &total_charges);
+
+        printf("Enter total credits: ");
+        scanf("%f", &total_credits);
+
+        printf("Enter credit limit: ");
+        scanf("%f", &credit_limit);
+
+        balance = beginning_balance + total_charges - total_credits;
+
+        if(balance > credit_limit){
+
+            printf("Account: %d\n", account_number);
+            printf("Credit limit: %f\n", credit_limit);
+            printf("Balance: %f\n", balance);
+
+            puts("Credit Limit Exceeded.");
+        }
+
+    }
+}
